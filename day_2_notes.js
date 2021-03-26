71a0175282b0dc9ad2f53276e7ae694faadda1fc
unknown <arianne.domka@gmail.com>
2021-03-25 21:05:56 -0400

    Initialize stuff

---
+++ day_2_notes.js
@@ -0,0 +1,46 @@
+// let name;
+// // name = 'Arianne'
+// let name = 'Ari!'
+
+// console.log(name)
+// name = 'Arianne';
+// console.log(name);
+
+// const number = 15
+// console.log(number)
+
+// var x = 10; var - function scoped
+// console.log(x) let - block scoped
+
+// console.log(x)
+// var x = 10
+
+// var is function scoped and declararion loads up before the file is read
+// let must be declared first before being called; else erros occurs
+
+// console.log(y)
+// let y = 15 error!
+
+// console.log(3 === "3")
+
+let myNum = 5;
+let myStr = '5';
+console.log(typeof myNum);
+console.log(typeof myStr);
+console.log(myNum + myStr)  
+
+// Git
+// Version control system designed to keep chronological timeline of all of your code changes.
+// To traverse file system in terminal:
+// cd name_of_folder -> move you down
+// cd .. -> moves you up
+// ls -> lists items inside existing name_of_folder
+// pwd -> specifies directory path you're currently in
+// pwd -> "print working directory"
+
+// To initiate a git repository
+// git init
+// Staging files for a commit (boxing up all your changes)
+// git add <file_name> OR git add . to add all of the files inside the folder that you are in
+// git status tells you the status of your repository
+// git commit -m "Your commit message here"
